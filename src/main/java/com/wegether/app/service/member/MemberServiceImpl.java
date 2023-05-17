be087267c6543e5fbfe3a0682ca64bce070d2d07
popUMe <youmip96@gmail.com>
2023-05-17 11:32:02 +0900

    박유미 / MVC 초기 세팅 MEMBER

---
+++ src/main/java/com/wegether/app/service/member/MemberServiceImpl.java
@@ -0,0 +1,10 @@
+package com.wegether.app.service.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+}
