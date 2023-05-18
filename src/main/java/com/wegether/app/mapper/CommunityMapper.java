6fbe77a2e1a419c9051e97dc3f508e1c4858799e
Developer-KCY <developer.kcy@gmail.com>
2023-05-18 12:55:52 +0900

    작업중

--- src/main/java/com/wegether/app/mapper/CommunityMapper.java
+++ src/main/java/com/wegether/app/mapper/CommunityMapper.java
@@ -1,7 +1,12 @@
 package com.wegether.app.mapper;
 
+import com.wegether.app.domain.vo.CommunityVO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
 
 @Mapper
 public interface CommunityMapper {
+
+    public Optional<CommunityVO> select(Long id);
 }
