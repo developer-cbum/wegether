4bed4cb9fff7d011599c930e66fe2ded4baf7e16
developer-cbum <dal.semicolon@gmail.com>
2023-05-20 02:29:21 +0900

    컨설팅 등록 작업

--- src/main/java/com/wegether/app/controller/ConsultController.java
+++ src/main/java/com/wegether/app/controller/ConsultController.java
@@ -1,17 +1,27 @@
 package com.wegether.app.controller;
 
+import com.wegether.app.domain.vo.ConsultingVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/consult/*")
+@RequestMapping("/consults/*")
 public class ConsultController {
 
+    @GetMapping("register")
+    public void goToRegisterForm(ConsultingVO consultingVO){};
+
+    @PostMapping("register")
+    public void register(){
+
+    }
 }
 
 
---
+++ src/test/java/com/wegether/app/mapper/consultings/consultingMapperTests.java
@@ -0,0 +1,4 @@
+package com.wegether.app.mapper.consultings;
+
+public class consultingMapperTests {
+}
