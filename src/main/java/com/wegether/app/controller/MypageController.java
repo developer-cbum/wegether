099ac970de2d0c40503f212db914117bc05bd4ac
INgenious-with <injin0318@gmail.com>
2023-05-19 19:02:09 +0900

    1

--- src/main/java/com/wegether/app/controller/MypageController.java
+++ src/main/java/com/wegether/app/controller/MypageController.java
@@ -1,5 +1,6 @@
 package com.wegether.app.controller;
 
+import com.wegether.app.service.mypage.MypageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/mypage/*")
 public class MypageController {
+
 
 }
 
