c1a0e30031bd2528a5a9ab7676bd30cf6f0b70d2
popUMe <youmip96@gmail.com>
2023-05-22 03:20:36 +0900

    file 썸네일 굿

---
+++ src/test/java/com/wegether/app/mapper/data/DataFileMapperTests.java
@@ -0,0 +1,46 @@
+package com.wegether.app.mapper.data;
+
+import com.wegether.app.domain.dto.CommunityFileDTO;
+import com.wegether.app.domain.dto.DataDTO;
+import com.wegether.app.domain.dto.DataFileDTO;
+import com.wegether.app.domain.type.FileType;
+import com.wegether.app.domain.vo.CommunityFileVO;
+import com.wegether.app.mapper.DataFileMapper;
+import com.wegether.app.mapper.FileMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+@SpringBootTest
+@Slf4j
+public class DataFileMapperTests {
+
+    @Autowired
+    private FileMapper fileMapper;
+
+    @Autowired
+    private DataFileMapper dataFileMapper;
+
+    @Test
+    public void insertTest() {
+        DataFileDTO dataFileDTO = new DataFileDTO();
+        dataFileDTO.setFileName("icon2.png");
+        dataFileDTO.setFilePath("2023/05/20");
+        dataFileDTO.setFileSize(999L);
+        dataFileDTO.setFileUuid(UUID.randomUUID().toString());
+        dataFileDTO.setFileType(FileType.REPRESENTATIVE.name());
+        fileMapper.insert(dataFileDTO);
+    }
+
+    @Test
+    public void dataSelectAllTest() {
+        fileMapper.dataSelectAll(1L).stream().map(DataFileDTO::toString).forEach(log::info);
+    }
+
+
+
+
+}
