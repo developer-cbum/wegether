7e65e2857b666915d1333301fe6d3e675563a45d
Developer-Gilbert <Gilbert.dev.kr.gmail.com>
2023-05-22 05:27:01 +0900

    풀받기

--- src/main/java/com/wegether/app/controller/ProjectController.java
+++ src/main/java/com/wegether/app/controller/ProjectController.java
@@ -1,5 +1,6 @@
 package com.wegether.app.controller;
 
+import com.wegether.app.domain.dto.ProjectDTO;
 import com.wegether.app.domain.dto.ProjectPagination;
 import com.wegether.app.service.project.ProjectService;
 import lombok.RequiredArgsConstructor;
@@ -7,7 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 import org.thymeleaf.model.IAttribute;
 
 
@@ -24,9 +29,36 @@ public class ProjectController {
         model.addAttribute("projects", projectService.getList(projectPagination));
     }
 
-//    @GetMapping
-//    public void
-//
+    @GetMapping("write")
+    public void goToRegisterForm(ProjectDTO projectDTO, Model model){;}
+
+    @PostMapping("write")
+    public RedirectView register(ProjectDTO projectDTO){
+        projectService.write(projectDTO);
+        return new RedirectView("/project/studio/write/baseinfo");
+    }
+
+
+
+    @GetMapping("modify")
+    public void goToProjectModify(@RequestParam Long id, Model model){
+        model.addAttribute("project", projectService.getProject(id).get());
+    }
+
+    @GetMapping("detail")
+    public void goToProjectDetail(@RequestParam Long id, Model model){
+        model.addAttribute("project", projectService.getProject(id).get());
+        log.info(projectService.getProject(1L).toString());
+    }
+
+    @PostMapping
+    public RedirectView modify(ProjectDTO projectDTO, RedirectAttributes redirectAttributes){
+        log.info(projectDTO.toString());
+        projectService.modify(projectDTO);
+        redirectAttributes.addAttribute("id", projectDTO.getId());
+        return new RedirectView("/project/detail");
+    }
+
 }
 
 
