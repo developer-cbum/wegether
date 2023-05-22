2211c2f200774c00792155d5363ce463a0dcb5d8
INgenious-with <injin0318@gmail.com>
2023-05-22 17:11:07 +0900

    5월 22일 김인진 작업

--- src/main/java/com/wegether/app/controller/AdminController.java
+++ src/main/java/com/wegether/app/controller/AdminController.java
@@ -27,9 +27,17 @@ public class AdminController {
     @GetMapping("notice/write")
     public void goToWriteForm(NoticeVO noticeVO){;}
 
-//    공지사항 목록 이동
+////    공지사항 목록 이동
+//    @GetMapping("notice/list")
+//    public void goToListForm(NoticeVO noticeVO){;}
+
+    //    공지사항 목록 이동
     @GetMapping("notice/list")
-    public void goToListForm(NoticeVO noticeVO){;}
+    public void noticeList(Model model, AdminPagination adminPagination){
+        adminPagination.setTotal(adminService.getNoticeTotal());
+        adminPagination.progress();
+        model.addAttribute("notices", adminService.noticeGetList(adminPagination));
+    }
 
 //    공지사항 상세 이동
     @GetMapping("notice/detail")
@@ -46,8 +54,15 @@ public class AdminController {
     public void goToWriteForm(InquiryVO inquiryVO){;}
 
     //    문의사항 목록 이동
+//    @GetMapping("inquiry/list")
+//    public void goToListForm(InquiryVO inquiryVO){;}
+
     @GetMapping("inquiry/list")
-    public void goToListForm(InquiryVO inquiryVO){;}
+    public void inquiryList(Model model, AdminPagination adminPagination){
+        adminPagination.setTotal(adminService.getInquiryTotal());
+        adminPagination.progress();
+        model.addAttribute("inquiries", adminService.inquiryGetList(adminPagination));
+    }
 
     //    문의사항 상세 이동
     @GetMapping("inquiry/detail")
@@ -63,42 +78,84 @@ public class AdminController {
 //    @GetMapping("data/list")
 //    public void goToListForm(DataAdminDTO dataAdminDTO){;}
 
+    //    자료 목록 이동
     @GetMapping("data/list")
-    public void list(Model model, AdminPagination adminPagination){
+    public void dataList(Model model, AdminPagination adminPagination){
         adminPagination.setTotal(adminService.getDataTotal());
         adminPagination.progress();
         model.addAttribute("datas", adminService.dataGetList(adminPagination));
     }
 
-    // 자료 삭제
+    //    자료 삭제
     @PostMapping("data/delete")
     @ResponseBody
-    public void remove(@RequestBody List<String> dataIds) {
+    public void dataRemove(@RequestBody List<String> dataIds) {
         for (String dataId : dataIds) adminService.dataRemove(Long.valueOf(dataId));
     }
 
-
-//    @PostMapping("data/delete")
-//    public RedirectView remove(Long id) { adminService.dataRemove(id); return new RedirectView("admins/data/list");}
-
-
 /* -------------------------------------------------------- */
+
+//    //    프로젝트 목록 이동
+//    @GetMapping("project/list")
+//    public void goToListForm(ProjectVO projectVO){;}
 
     //    프로젝트 목록 이동
     @GetMapping("project/list")
-    public void goToListForm(ProjectVO projectVO){;}
+    public void projectList(Model model, AdminPagination adminPagination){
+        adminPagination.setTotal(adminService.getProjectTotal());
+        adminPagination.progress();
+        model.addAttribute("projects", adminService.projectGetList(adminPagination));
+    }
+
+    //    프로젝트 삭제
+    @PostMapping("project/delete")
+    @ResponseBody
+    public void projectRemove(@RequestBody List<String> projectIds) {
+        for (String projectId : projectIds) adminService.projectRemove(Long.valueOf(projectId));
+    }
 
 /* -------------------------------------------------------- */
 
     //    강연 목록 이동
+//    @GetMapping("lecture/list")
+//    public void goToListForm(LectureVO lectureVO){;}
+
+    //    강연 목록 이동
     @GetMapping("lecture/list")
-    public void goToListForm(LectureVO lectureVO){;}
+    public void lectureList(Model model, AdminPagination adminPagination){
+        adminPagination.setTotal(adminService.getLectureTotal());
+        adminPagination.progress();
+        model.addAttribute("lectures", adminService.lectureGetList(adminPagination));
+    }
+
+    //    강연 삭제
+    @PostMapping("lecture/delete")
+    @ResponseBody
+    public void lectureRemove(@RequestBody List<String> lectureIds) {
+        for (String lectureId : lectureIds) adminService.lectureRemove(Long.valueOf(lectureId));
+    }
+    
 
 /* -------------------------------------------------------- */
 
     //    회원 목록 이동
+//    @GetMapping("member/list")
+//    public void goToListForm(MemberVO memberVO){;}
+
+    //    회원 목록 이동
     @GetMapping("member/list")
-    public void goToListForm(MemberVO memberVO){;}
+    public void MemberList(Model model, AdminPagination adminPagination){
+        adminPagination.setTotal(adminService.getMemberTotal());
+        adminPagination.progress();
+        model.addAttribute("members", adminService.memberGetList(adminPagination));
+    }
+
+    //    회원 상태 변경
+    @PostMapping("member/delete")
+    @ResponseBody
+    public void memberRemove(@RequestBody List<String> memberIds) {
+        for (String memberId : memberIds) adminService.memberRemove(Long.valueOf(memberId));
+    }
 
 /* -------------------------------------------------------- */
 
